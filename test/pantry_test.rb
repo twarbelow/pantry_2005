135c1142c43e6e4f4822e380f5ac7676796646cb
twarbelow <taija.warbelow@google.com>
2020-06-03 09:27:27 -0700

    add restock test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -18,4 +18,9 @@ class PantryTest < Minitest::Test
     assert_equal 0, @pantry.stock_check(@ingredient1)
   end
 
+  def test_it_can_restock
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+  end
 end
