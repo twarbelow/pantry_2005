4f2a65b6aefdba03559d14c2ab58a99c0fd06e8f
twarbelow <taija.warbelow@google.com>
2020-06-03 10:13:40 -0700

    add knows ingredients test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -9,6 +9,8 @@ class CookBookTest < MiniTest::Test
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
   end
@@ -17,6 +19,18 @@ class CookBookTest < MiniTest::Test
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
     assert_equal ([@recipe1, @recipe2]), @cookbook.recipes
+  end
+
+  def test_it_knows_ingredients
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    assert_equal (["Cheese", "Macaroni", "Ground Beef", "Bun"]), @cookbook.ingredients
   end
 
 end
