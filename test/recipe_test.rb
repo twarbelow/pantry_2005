7c4d08c9ecf2e8e17097ca397fd5353a09ccb16b
twarbelow <taija.warbelow@google.com>
2020-06-03 11:03:39 -0700

    started outlining details test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -43,4 +43,11 @@ class RecipeTest < MiniTest::Test
 
     assert_equal 675, @recipe2.total_calories
   end
+
+  def test_it_has_details
+    skip
+    assert_equal ({:name => "Mac and Cheese", :details => {@ingredient1 => "Cheese", :amount => "2 C"}, {@ingredient2 => "Macaroni", :amount => "8 oz"}}), @recipe1.details
+    #  details => {:ingredient1 => "name",
+    # :amount => "amount unit"}
+  end
 end
