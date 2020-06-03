af0e935ed4306a91770d51280d62e7c2ca5c5568
twarbelow <taija.warbelow@google.com>
2020-06-03 10:05:09 -0700

    update setup, add total calories test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -8,6 +8,9 @@ class RecipeTest < MiniTest::Test
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   def test_it_has_a_name
@@ -27,5 +30,17 @@ class RecipeTest < MiniTest::Test
     @recipe1.add_ingredient(@ingredient1, 4)
     @recipe1.add_ingredient(@ingredient2, 8)
     assert_equal ([@ingredient1, @ingredient2]), @recipe1.ingredients
+  end
+
+  def test_it_has_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    assert_equal 440, @recipe1.total_calories
+
+    assert_equal 675, @recipe2.total_calories
   end
 end
