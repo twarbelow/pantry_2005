fd511d2b6fb2a75a42853e9860cda2436d51196b
twarbelow <taija.warbelow@google.com>
2020-06-03 09:50:29 -0700

    create add ingredient method and private attr_writer

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,4 +6,14 @@ class Recipe
     @ingredients_required = {}
   end
 
+  def add_ingredient(ingredient, amount)
+    if ingredients_required[ingredient].nil?
+      ingredients_required[ingredient] = amount
+    else
+      ingredients_required[ingredient] += amount
+    end
+  end
+
+  private
+  attr_writer :ingredients_required
 end
