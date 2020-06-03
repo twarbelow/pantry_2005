c40316a0565e917fd3c84191dc32dbda7b350688
twarbelow <taija.warbelow@google.com>
2020-06-03 09:18:23 -0700

    add calories

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,10 +1,10 @@
 class Ingredient
-  attr_reader :name, :unit
+  attr_reader :name, :unit, :calories
 
   def initialize(info)
     @name = info[:name]
     @unit = info[:unit]
-
+    @calories = info[:calories]
   end
 
 end
