e35e6de8ba23efb2c713bf2ca030bbcf2a2ba632
twarbelow <taija.warbelow@google.com>
2020-06-03 09:54:09 -0700

    add ingredients to reader, writer, init, and add ingredient method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,19 +1,21 @@
 class Recipe
-  attr_reader :name, :ingredients_required
+  attr_reader :name, :ingredients_required, :ingredients
 
   def initialize(name)
     @name = name
     @ingredients_required = {}
+    @ingredients = []
   end
 
   def add_ingredient(ingredient, amount)
     if ingredients_required[ingredient].nil?
       ingredients_required[ingredient] = amount
+      ingredients << ingredient
     else
       ingredients_required[ingredient] += amount
     end
   end
 
   private
-  attr_writer :ingredients_required
+  attr_writer :ingredients_required, :ingredients
 end
