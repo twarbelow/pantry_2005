506a64579fda9c9727531de0c117c690879d9deb
twarbelow <taija.warbelow@google.com>
2020-06-03 09:42:10 -0700

    setup recipe test file and add name test

---
+++ test/recipe_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < MiniTest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_has_a_name
+    assert_equal "Mac and Cheese", @recipe1.name
+  end
+end
