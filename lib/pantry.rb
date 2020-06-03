ef3580ec36f2362aa9e967e974f3fb6ab697e828
twarbelow <taija.warbelow@google.com>
2020-06-03 09:31:51 -0700

    add restock method, update stock_check method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -7,6 +7,12 @@ class Pantry
 
   def stock_check(ingredient)
     stock[:ingredient] = 0 if stock[:ingredient].nil?
+    stock[:ingredient]
+  end
+
+  def restock(ingredient, amount)
+    stock_check(ingredient)
+    stock[:ingredient] += amount
   end
 
 end
