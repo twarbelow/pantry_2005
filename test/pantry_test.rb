826a3c62b470ddcd0587a97682d5415350303d40
twarbelow <taija.warbelow@google.com>
2020-06-03 10:23:00 -0700

    update setup, add enough ingredients test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,11 +2,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @recipe1 = Recipe.new("Mac and Cheese")
     @pantry = Pantry.new
   end
 
@@ -22,5 +24,24 @@ class PantryTest < Minitest::Test
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
     assert_equal 15, @pantry.stock_check(@ingredient1)
+
+    @pantry.restock(@ingredient2, 7)
+
+    assert_equal 7, @pantry.stock_check(@ingredient2)
+  end
+
+  def test_it_has_enough_ingredients_for?
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+
+    @pantry.restock(@ingredient2, 7)
+
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+
+    @pantry.restock(@ingredient2, 1)
+
+    assert_equal true, @pantry.enough_ingredients_for?(@recipe1)
   end
 end
