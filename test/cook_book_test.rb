a16c02d9a4736b888fdb4480d7ebb12d210c10f2
twarbelow <taija.warbelow@google.com>
2020-06-03 10:17:36 -0700

    add highest calorie meal test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -33,4 +33,16 @@ class CookBookTest < MiniTest::Test
     assert_equal (["Cheese", "Macaroni", "Ground Beef", "Bun"]), @cookbook.ingredients
   end
 
+  def test_it_knows_highest_calorie_meal
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    assert_equal @recipe2, @cookbook.highest_calorie_meal
+  end
+
 end
