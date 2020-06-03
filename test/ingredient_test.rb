e2c53a573ba110bb578aa5ce390f99662e0344fa
twarbelow <taija.warbelow@google.com>
2020-06-03 11:04:29 -0700

    all tests passing

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -5,9 +5,7 @@ require './lib/pantry'
 
 class IngredientTest < Minitest::Test
   def setup
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-  end
-
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})  end
   def test_it_has_a_name
     assert_equal "Cheese", @ingredient1.name
   end
